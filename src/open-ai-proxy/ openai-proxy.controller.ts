0ff97a7fc2b038b08bf6548123bed357935dc90f
VeeDock <hatik777@gmail.com>
2025-04-30 15:09:29 +0300

    openai client

--- src/open-ai-proxy/ openai-proxy.controller.ts
+++ src/open-ai-proxy/ openai-proxy.controller.ts
@@ -1,18 +1,26 @@
 import { Controller, Req, Res, All } from '@nestjs/common';
 import { Request, Response } from 'express';
-import axios from 'axios';
+import { HttpService } from '@nestjs/axios';
+import { firstValueFrom } from 'rxjs';
+import axios, { AxiosRequestConfig } from 'axios';
+import OpenAI from 'openai';
+import { FinalRequestOptions } from 'openai/core';
+import * as QueryString from 'node:querystring';
 
 @Controller('v1')
 export class OpenAiProxyController {
   private readonly openaiBase = 'https://api.openai.com';
+  private openai: OpenAI;
 
-  constructor() {}
+  constructor(private readonly httpService: HttpService) {
+    this.openai = new OpenAI({ apiKey: '' });
+  }
 
   @All('*proxy')
   async proxy(@Req() req: Request, @Res() res: Response) {
     const targetUrl = `${this.openaiBase}${req.url}`;
 
-    console.log('method', req.method)
+    console.log('method', req.method);
     console.log('url', targetUrl);
     console.log('body', req.body);
 
@@ -21,25 +29,39 @@ export class OpenAiProxyController {
         ...req.headers,
         host: 'api.openai.com',
       };
+      if ('set-cookie' in headers) {
+        delete headers['set-cookie'];
+      }
 
-      const response = await axios({
+      const conf: FinalRequestOptions<any> = {
         method: req.method as any,
-        url: targetUrl,
-        headers,
-        data: req.body,
-        responseType:
-          req.headers.accept === 'text/event-stream' ? 'stream' : 'json',
-      });
+        path: targetUrl,
+        headers: {
+          Host: 'api.openai.com',
+          Authorization: headers.authorization,
+          'OpenAI-Beta': headers['OpenAI-Beta'],
+          'Content-Type': headers['Content-Type'],
+        },
+        // data: req.body,
+        // responseType:
+        //   req.headers.accept === 'text/event-stream' ? 'stream' : 'json',
+      };
 
-      // console.log('response', response)
+      const response = await this.openai.request<
+        Request<any>,
+        Response<Response>
+      >(conf);
+      console.log('response', response);
+
+      // const response = await axios();
 
       // SSE поддержка
-      if (response.headers['content-type']?.includes('text/event-stream')) {
+      if (response.header['content-type']?.includes('text/event-stream')) {
         res.setHeader('Content-Type', 'text/event-stream');
-        console.log('stream');
+        // @ts-ignore
         response.data.pipe(res);
       } else {
-        console.log('send response');
+        // @ts-ignore
         res.status(response.status).json(response.data);
       }
     } catch (err) {
--- src/open-ai-proxy/open-ai-proxy.module.ts
+++ src/open-ai-proxy/open-ai-proxy.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { OpenAiProxyController } from './ openai-proxy.controller';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [HttpModule],
   controllers: [OpenAiProxyController],
 })
 export class OpenAiProxyModule {}
