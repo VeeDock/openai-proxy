0a86f8573fc51357d50a8276091b63618df12b26
VeeDock <hatik777@gmail.com>
2025-04-30 12:20:04 +0300

    config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OpenAiProxyModule } from './open-ai-proxy/open-ai-proxy.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [OpenAiProxyModule],
+  imports: [
+    OpenAiProxyModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
