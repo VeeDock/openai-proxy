0aefa148ef2db4286c0c90d130d07537f9558e47
VeeDock <hatik777@gmail.com>
2025-04-30 21:30:29 +0300

    openai client

--- src/open-ai-proxy/ openai-proxy.controller.ts
+++ src/open-ai-proxy/ openai-proxy.controller.ts
@@ -55,6 +55,7 @@ export class OpenAiProxyController {
       //
       response.on('end', function () {
         console.log('ended');
+        res.end();
         // const body = Buffer.concat(chunks);
         // // console.log(body.toString());
         // res.json(JSON.parse(body.toString()));
