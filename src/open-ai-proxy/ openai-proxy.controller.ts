8c55f876729e4e490917ce87111cd013d9d5a953
VeeDock <hatik777@gmail.com>
2025-05-02 13:52:30 +0300

    openai client

--- src/open-ai-proxy/ openai-proxy.controller.ts
+++ src/open-ai-proxy/ openai-proxy.controller.ts
@@ -1,28 +1,25 @@
 import { Controller, Req, Res, All } from '@nestjs/common';
 import { Request, Response } from 'express';
 import * as https from 'node:https';
-import * as http from 'node:http';
 
-function normalizeHeaders(
-  headers: http.IncomingHttpHeaders,
-): Map<string, string> {
-  const normalized: Map<string, string> = new Map();
-
-  for (const [key, value] of Object.entries(headers)) {
-    if (typeof value === 'string') {
-      normalized[key] = value;
-      normalized.set(key, value);
-    } else if (Array.isArray(value)) {
-      normalized[key] = value.join(','); // или берёшь value[0], если нужен один
-      normalized.set(key, value.join(','));
-    } else if (typeof value === 'number') {
-      // normalized[key] = value.toString();
-      normalized.set(key, value);
-    }
-  }
-
-  return normalized;
-}
+// function normalizeHeaders(headers: IncomingHttpHeaders): Map<string, string> {
+//   const normalized: Map<string, string> = new Map();
+//
+//   for (const [key, value] of Object.entries(headers)) {
+//     if (typeof value === 'string') {
+//       normalized[key] = value;
+//       normalized.set(key, value);
+//     } else if (Array.isArray(value)) {
+//       normalized[key] = value.join(','); // или берёшь value[0], если нужен один
+//       normalized.set(key, value.join(','));
+//     } else if (typeof value === 'number') {
+//       // normalized[key] = value.toString();
+//       normalized.set(key, value);
+//     }
+//   }
+//
+//   return normalized;
+// }
 
 function sendMessage(res: Response, mes: string) {
   console.log('sending', new Date());
@@ -87,12 +84,9 @@ export class OpenAiProxyController {
 
       // res.setHeader('Connection', 'keep-alive');
       // response.headers['content-type'] = 'text/event-stream';
-
+      res.writeHead(response.statusCode || 500, response.headers);
       if (isStream) {
-        res.writeHead(response.statusCode || 500, response.headers);
         res.flushHeaders();
-      } else {
-        res.setHeaders(normalizeHeaders(response.headers));
       }
 
       //
@@ -127,8 +121,8 @@ export class OpenAiProxyController {
         console.log('ended!');
         if (!isStream) {
           const body = Buffer.concat(chunks);
-          res.json(body.toString());
-          // res.write(body.toString());
+          // res.json(body);
+          res.write(body.toString());
         }
         // res.json();
         res.end();
