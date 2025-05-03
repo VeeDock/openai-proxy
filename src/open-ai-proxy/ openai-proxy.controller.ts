61b3590925ecfa1b59f466ff72e09b255cf185aa
VeeDock <hatik777@gmail.com>
2025-05-03 04:56:47 +0300

    openai client

--- src/open-ai-proxy/ openai-proxy.controller.ts
+++ src/open-ai-proxy/ openai-proxy.controller.ts
@@ -105,13 +105,14 @@ export class OpenAiProxyController {
     });
     // const rawHeaders = response.headers.toJSON;
     // console.log('raw', rawHeaders);
-    // res.json(await response.json());
-    if (response.body) {
-      const nodeReadable = webStreamToNodeReadable(response.body);
-      nodeReadable.pipe(res);
-    } else {
-      res.send();
-    }
+    const data = await response.json();
+    res.json(data);
+    // if (response.body) {
+    //   const nodeReadable = webStreamToNodeReadable(response.body);
+    //   nodeReadable.pipe(res);
+    // } else {
+    //   res.send();
+    // }
   }
 
   @All('test/*proxy')
